25ccfbddff3923ba8bfc1122742c54106642a7be
Nima Lama <nima.themegrill@gmail.com>
2017-06-12 15:00:29 +0545

    Added demo config file theme masonic

---
+++ resources/masonic/masonic-free/tg-demo-config.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Functions for configuring demo importer.
+ *
+ * @author   ThemeGrill
+ * @category Admin
+ * @package  Importer/Functions
+ * @version  1.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Setup demo importer config.
+ *
+ * @param  array $demo_config
+ * @return array
+ */
+function masonic_demo_importer_config( $demo_config ) {
+	$new_demo_config = array(
+		'masonic-free' => array(
+			'name'         => 'Masonic',
+			'theme'        => 'Masonic',
+			'template'     => 'masonic',
+			'demo_url'     => 'https://demo.themegrill.com/masonic/',
+			'demo_pack'    => true,
+			'core_options' => array(
+				'blogname'       => 'Masonic',
+			),
+			'customizer_data_update' => array(
+				'nav_menu_locations' => array(
+					'primary' => 'Primary Menu',
+				),
+			),
+			'plugins_list' => array(
+				'recent-posts-widget-extended' => array(
+					'name'     => 'Recent Posts Widget Extended',
+					'slug'     => 'recent-posts-widget-extended/rpwe.php',
+					'required' => false,
+				),
+				'contact-form-7' => array(
+					'name'     => 'Contact Form 7',
+					'slug'     => 'contact-form-7/wp-contact-form-7.php',
+					'required' => false,
+				),
+			),
+		),
+	);
+
+	return array_merge( $new_demo_config, $demo_config );
+}
+
+add_filter( 'themegrill_demo_importer_config', 'masonic_demo_importer_config' );
