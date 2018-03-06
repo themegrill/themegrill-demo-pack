a6beed23399547a5a460a2548f6144be8dad045f
Ashish Shrestha <ashishsthanp@gmail.com>
2018-03-06 15:58:56 +0545

    Updated blog data and config

---
+++ resources/spacious/spacious-blog/tg-demo-config.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Demo Name: Spacious Blog
+ * Demo URI: http://demo.themegrill.com/spacious-blog/
+ * Description: Spacious Free demo pack for ThemeGrill Official theme.
+ * Version: 1.0.0
+ * Template: spacious
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * Setup demo importer config.
+ *
+ * @param  array $demo_config
+ *
+ * @return array
+ */
+function spacious_demo_importer_config( $demo_config ) {
+	$demo_config['spacious-blog'] = array(
+		'name'                   => 'Spacious Blog',
+		'theme'                  => 'Spacious',
+		'template'               => 'spacious',
+		'demo_url'               => 'http://demo.themegrill.com/spacious-blog/',
+		'demo_pack'              => true,
+		'core_options'           => array(
+			'blogname'      => 'Spacious Blog',
+			'page_on_front' => 'Home',
+		),
+		'customizer_data_update' => array(
+			'nav_menu_locations' => array(
+				'primary' => 'primary',
+			),
+		),
+		'plugins_list'           => array(
+			'elementor'        => array(
+				'name'     => 'Elementor',
+				'slug'     => 'elementor/elementor.php',
+				'required' => true,
+			),
+			'spacious-toolkit' => array(
+				'name'     => 'Spacious Toolkit',
+				'slug'     => 'spacious-toolkit/spacious-toolkit.php',
+				'required' => true,
+			),
+			'everest-forms'    => array(
+				'name'     => 'Everest Forms – Easy Contact Form and Form Builder',
+				'slug'     => 'everest-forms/everest-forms.php',
+				'required' => false,
+			),
+		),
+	);
+
+	return $demo_config;
+}
+
+add_filter( 'themegrill_demo_importer_config', 'spacious_demo_importer_config' );
