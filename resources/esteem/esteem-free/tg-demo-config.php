84b81792625254c14af9ffc3120b1295f8dd55a6
Bishal Napit <mebishalnapit@gmail.com>
2018-03-21 11:51:39 +0545

    Update Esteem free demo dummy data xml file

--- resources/esteem/esteem-free/tg-demo-config.php
+++ resources/esteem/esteem-free/tg-demo-config.php
@@ -15,21 +15,22 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Setup demo importer config.
  *
  * @param  array $demo_config
- * @return arrayesteem_demo_importer_config
+ *
+ * @return array esteem_demo_importer_config
  */
 function esteem_demo_importer_config( $demo_config ) {
 	$demo_config['esteem-free'] = array(
-		'name'         => 'Esteem Free',
-		'theme'        => 'Esteem',
-		'template'     => 'esteem',
-		'demo_url'     => 'https://demo.themegrill.com/esteem/',
-		'demo_pack'    => true,
-		'core_options' => array(
+		'name'                   => 'Esteem Free',
+		'theme'                  => 'Esteem',
+		'template'               => 'esteem',
+		'demo_url'               => 'https://demo.themegrill.com/esteem/',
+		'demo_pack'              => true,
+		'core_options'           => array(
 			'blogname'       => 'Esteem',
 			'page_on_front'  => 'Business',
 			'page_for_posts' => 'Blog',
 		),
-		'widgets_data_update' => array(
+		'widgets_data_update'    => array(
 			/**
 			 * Dropdown Pages - Handles widgets Page ID.
 			 *
@@ -37,7 +38,7 @@ function esteem_demo_importer_config( $demo_config ) {
 			 * 2. esteem_recent_work_widget
 			 */
 			'dropdown_pages' => array(
-				'esteem_service_widget' => array(
+				'esteem_service_widget'     => array(
 					2 => array(
 						'page_id0' => 'Responsive Design',
 						'page_id1' => 'Retina Ready',
@@ -59,11 +60,11 @@ function esteem_demo_importer_config( $demo_config ) {
 				'primary' => 'Primary',
 			),
 		),
-		'plugins_list' => array(
-			'contact-form-7' => array(
-				'name'     => 'Contact Form 7',
-				'slug'     => 'contact-form-7/wp-contact-form-7.php',
-				'required' => false,
+		'plugins_list'           => array(
+			'everest-forms'    => array(
+				'name'     => 'Everest Forms – Easy Contact Form and Form Builder',
+				'slug'     => 'everest-forms/everest-forms.php',
+				'required' => true,
 			),
 			'breadcrumb-navxt' => array(
 				'name'     => 'Breadcrumb NavXT',
@@ -75,4 +76,5 @@ function esteem_demo_importer_config( $demo_config ) {
 
 	return $demo_config;
 }
+
 add_filter( 'themegrill_demo_importer_config', 'esteem_demo_importer_config' );
