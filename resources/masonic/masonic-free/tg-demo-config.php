b1fbec1e89c53d78957121de647f945439f19d2c
Bishal Napit <mebishalnapit@gmail.com>
2018-03-22 16:10:15 +0545

    Update dummy demo data for Masonic free theme

--- resources/masonic/masonic-free/tg-demo-config.php
+++ resources/masonic/masonic-free/tg-demo-config.php
@@ -15,34 +15,35 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Setup demo importer config.
  *
  * @param  array $demo_config
+ *
  * @return array
  */
 function masonic_demo_importer_config( $demo_config ) {
 	$new_demo_config = array(
 		'masonic-free' => array(
-			'name'         => 'Masonic',
-			'theme'        => 'Masonic',
-			'template'     => 'masonic',
-			'demo_url'     => 'https://demo.themegrill.com/masonic/',
-			'demo_pack'    => true,
-			'core_options' => array(
-				'blogname'       => 'Masonic',
+			'name'                   => 'Masonic',
+			'theme'                  => 'Masonic',
+			'template'               => 'masonic',
+			'demo_url'               => 'https://demo.themegrill.com/masonic/',
+			'demo_pack'              => true,
+			'core_options'           => array(
+				'blogname' => 'Masonic',
 			),
 			'customizer_data_update' => array(
 				'nav_menu_locations' => array(
 					'primary' => 'Primary Menu',
 				),
 			),
-			'plugins_list' => array(
+			'plugins_list'           => array(
 				'recent-posts-widget-extended' => array(
 					'name'     => 'Recent Posts Widget Extended',
 					'slug'     => 'recent-posts-widget-extended/rpwe.php',
 					'required' => false,
 				),
-				'contact-form-7' => array(
-					'name'     => 'Contact Form 7',
-					'slug'     => 'contact-form-7/wp-contact-form-7.php',
-					'required' => false,
+				'everest-forms'                => array(
+					'name'     => 'Everest Forms – Easy Contact Form and Form Builder',
+					'slug'     => 'everest-forms/everest-forms.php',
+					'required' => true,
 				),
 			),
 		),
