fd7061625be1c4e04e0b9d0e532727adb2e300ba
Bishal Napit <mebishalnapit@gmail.com>
2018-09-27 12:26:17 +0545

    Update Ample Free demo config for category select in customize option

--- resources/ample/ample-free/tg-demo-config.php
+++ resources/ample/ample-free/tg-demo-config.php
@@ -42,8 +42,8 @@ function ample_demo_importer_config( $demo_config ) {
 				'category' => array(
 					'ample_portfolio_widget' => array(
 						3 => array(
-							'category' => 'Portfolio'
-						)
+							'category' => 'Portfolio',
+						),
 					),
 				),
 			),
@@ -97,3 +97,39 @@ function ample_demo_importer_config( $demo_config ) {
 }
 
 add_filter( 'themegrill_demo_importer_config', 'ample_demo_importer_config' );
+
+/**
+ * Set categories checkbox to hide from blog page settings in theme customizer.
+ *
+ * Note: Used rarely, if `ample_option` keys are based on term ID.
+ *
+ * @param  array  $data
+ * @param  array  $demo_data
+ * @param  string $demo_id
+ *
+ * @return array
+ */
+function tg_ample_set_cat_hide( $data, $demo_data, $demo_id ) {
+	// Format the data based on demo ID.
+	switch ( $demo_id ) {
+		case 'ample-free':
+			$wp_categories = array(
+				5 => 'Portfolio',
+			);
+			break;
+	}
+
+	// Fetch categories selected and assign it to new category generated on demo import.
+	foreach ( $wp_categories as $term_id => $term_name ) {
+		if ( ! empty( $data['options']['ample[ample_hide_category]'] ) ) {
+			$term = get_term_by( 'name', $term_name, 'category' );
+			if ( is_object( $term ) && $term->term_id ) {
+				$data['options']['ample[ample_hide_category]'] = $term->term_id;
+			}
+		}
+	}
+
+	return $data;
+}
+
+add_filter( 'themegrill_customizer_demo_import_settings', 'tg_ample_set_cat_hide', 20, 3 );
